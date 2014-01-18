ccc26f691dfcff0e206c8c4093dcabd811e1d702
sanyuancap <sanyuancap@foxmail.com>
2014-01-17 17:51:50 -0800

    Initial Commit

---
+++ test/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  test
+//
+//  Created by CC on 14-1-17.
+//  Copyright (c) 2014年 user. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
